b0e871fa31dfafce37d750d624d8055c73ee6673
Wille Faler <wille.faler@gmail.com>
2024-02-04 16:19:52 +0100

    cleanup ansible stuff

--- pkg/provider/ansible_provider.go
+++ pkg/provider/ansible_provider.go
@@ -2,6 +2,7 @@ package provider
 
 import (
 	"context"
+	"os"
 
 	"github.com/OpenPaasDev/openpaas/pkg/ansible"
 )
@@ -10,7 +11,6 @@ type Ansible struct{}
 
 type AnsibleConfig struct {
 	SudoUser   string            `yaml:"sudo_user"`
-	SSHKeyPath string            `yaml:"ssh_key_path"`
 	GlobalVars map[string]string `yaml:"global_vars"`
 	Playbooks  []Playbook        `yaml:"playbooks"`
 }
@@ -21,5 +21,31 @@ type Playbook struct {
 }
 
 func (s *Ansible) Run(ctx context.Context, providerConfig interface{}, inventory *ansible.Inventory) error {
+	conf, err := asAnsibleConfig(providerConfig)
+	if err != nil {
+		return err
+	}
+	ansibleClient := ansible.NewClient(inventory.Path, conf.SudoUser)
+	for _, playbook := range conf.Playbooks {
+		varsFile, err := generateVarsFile(playbook.Vars, conf.GlobalVars)
+		if err != nil {
+			return err
+		}
+		if varsFile != "" {
+			defer os.Remove(varsFile) //nolint
+		}
+		err = ansibleClient.Run(playbook.Name, varsFile)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
+}
+
+func asAnsibleConfig(providerConfig interface{}) (*AnsibleConfig, error) {
+	return nil, nil
+}
+
+func generateVarsFile(vars map[string]string, globalVars map[string]string) (string, error) {
+	return "", nil
 }
