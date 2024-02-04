580cca1495aa1cb0f2255639f545bc3eae2fef5a
Pere Villega <pvillega@aracon.com>
2024-02-04 21:29:03 +0100

    Fix doc style to comply with Go rules
    Signed-off-by: Pere Villega <pvillega@aracon.com>

--- pkg/platform/platform.go
+++ pkg/platform/platform.go
@@ -6,8 +6,8 @@ import (
 )
 
 type Platform interface {
-	// intended for changes to the platform that we want to run before any terraform or ansible script. Example: modify allowed ssh keys
+	// Preparation is intended for changes to the platform that we want to run before any terraform or ansible script. Example: modify allowed ssh keys
 	Preparation(context.Context, *conf.Config) error
-	// intended for cleanup after we run terraform and ansible scripts.
+	// Cleanup is intended for cleanup after we run terraform and ansible scripts.
 	Cleanup(context.Context, *conf.Config) error
 }
