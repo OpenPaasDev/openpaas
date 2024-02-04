a1b44d8f538af120e0bcd2e16174dc969a6b4db0
Pere Villega <pvillega@aracon.com>
2024-02-04 18:52:58 +0100

    Add logic to laod ssh keys into teh config
    Signed-off-by: Pere Villega <pvillega@aracon.com>

--- pkg/conf/github_keys.go
+++ pkg/conf/github_keys.go
@@ -11,7 +11,41 @@ import (
 	"strings"
 )
 
-func FetchGitHubKeys(githubUser string) ([]string, error) {
+func UpdateConfigWithGithubKeys(cnf *Config) (*Config, error) {
+	// get the github ids in the config, retrieve the keys
+	for _, user := range cnf.CloudProviderConfig.GithubIds {
+		keys, err := fetchGitHubKeys(user)
+		if err != nil {
+			return nil, err
+		}
+		// convert the keys to fingerprints
+		var fingerprints []string
+		for _, key := range keys {
+			fprint, err := generateFingerprint(key)
+			if err != nil {
+				return nil, err
+			}
+			fingerprints = append(fingerprints, fprint)
+		}
+		// add the fingerprints to the config
+		if sshKeys, ok := cnf.CloudProviderConfig.ProviderSettings["ssh_keys"]; ok {
+			// checks the value in the map is a slice of string
+			switch keys := sshKeys.(type) {
+			case []string:
+				cnf.CloudProviderConfig.ProviderSettings["ssh_keys"] = append(keys, fingerprints...)
+			default:
+				// Handle the case where ssh_keys exists but is not a slice of strings, which would be wrong
+				cnf.CloudProviderConfig.ProviderSettings["ssh_keys"] = fingerprints
+			}
+		} else {
+			// Create ssh_keys with the new keys if it doesn't exist
+			cnf.CloudProviderConfig.ProviderSettings["ssh_keys"] = fingerprints
+		}
+	}
+	return cnf, nil
+}
+
+func fetchGitHubKeys(githubUser string) ([]string, error) {
 	url := "https://github.com/" + githubUser + ".keys"
 	resp, err := http.Get(url)
 	if err != nil {
@@ -33,7 +67,7 @@ func FetchGitHubKeys(githubUser string) ([]string, error) {
 	return strings.Split(string(body), "\n"), nil
 }
 
-func GenerateFingerprint(key string) (string, error) {
+func generateFingerprint(key string) (string, error) {
 	publicKey, _, _, _, err := ssh.ParseAuthorizedKey([]byte(key))
 	if err != nil {
 		return "", err
--- pkg/conf/github_keys_test.go
+++ pkg/conf/github_keys_test.go
@@ -2,11 +2,65 @@ package conf
 
 import (
 	"errors"
+	"github.com/stretchr/testify/require"
+	"path/filepath"
 	"testing"
 
 	"github.com/jarcoal/httpmock"
 	"github.com/stretchr/testify/assert"
 )
+
+func UpdateConfigWithGithubKeys_NoKeysInConfig(t *testing.T) {
+	// Activate httpmock
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	// Mock response
+	payload := `ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH0p7vluS7ldSFDBYx9ZXVQcsJdWIoSTVvqhcakKDQ34 user@example.com
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCas+tCHuRci1xIHwkBFvrq/dDm0l3PSBiD+Pm7SOqq23Qg+ZUANcVNSgot0W/NmEHy/9rA78Ps+jHwrwPrliw6TNLYC82LueJHPo1dGioprmKVKn9efYVuFDUubRPr+/CAZXARUOSqMon7xiaxAy51qhIpWrLxcs2HP/G3IW8kVxuwdztyT7D+tz5tfCvH98PlXf6MjWudL8bbTAWU7OEpUVia2pcUlAOXkkOi0ANrx4Ieovmhw7G8/AC0Rn+g3hSf1A45RsODVFq9BezunWbjcNicwV2++/CFpE5fuXT6pRgrBfXgI3P4BVRMxaG4CXLl6uUPrg/8oYoz/uJtxzEwv767YeNICi9RfXjIg0hQLoZfIAZCxYIVZw9A91ZIG8+IP276gG1kHfyMfs2W95dK6Uy/fdF6G3p3PLFUtjSK6dZwQeO4IzltrxujQ26kgMFDYMmD7lDDI3JzLqXy969MpMd60iamXDpgxQ3okZpa7sd5TgtEH+aA8ia58bhSOwE= user@main.com`
+
+	httpmock.RegisterResponder("GET", "https://github.com/exampleUser.keys",
+		httpmock.NewStringResponder(200, payload))
+
+	// initial config without ssh_keys entry defined
+	conf, err := Load(filepath.Join("..", "testdata", "config.yaml"))
+	require.NoError(t, err)
+	assert.NotNil(t, conf)
+
+	// remove ssh keys defined
+	conf.CloudProviderConfig.ProviderSettings["ssh_keys"] = []string{}
+	expected := []string{"ae:dc:ab:c1:b1:b0:21:2b:8a:06:77:ae:9c:9b:4b:22", "a6:1e:2b:44:9c:84:e2:1c:84:9c:6d:2d:ed:72:ad:16"}
+
+	conf, err = UpdateConfigWithGithubKeys(conf)
+	assert.NoError(t, err)
+	assert.Contains(t, conf.CloudProviderConfig.ProviderSettings["ssh_keys"], expected)
+}
+
+func UpdateConfigWithGithubKeys_KeysInConfig(t *testing.T) {
+	// Activate httpmock
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	// Mock response
+	payload := `ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH0p7vluS7ldSFDBYx9ZXVQcsJdWIoSTVvqhcakKDQ34 user@example.com
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCas+tCHuRci1xIHwkBFvrq/dDm0l3PSBiD+Pm7SOqq23Qg+ZUANcVNSgot0W/NmEHy/9rA78Ps+jHwrwPrliw6TNLYC82LueJHPo1dGioprmKVKn9efYVuFDUubRPr+/CAZXARUOSqMon7xiaxAy51qhIpWrLxcs2HP/G3IW8kVxuwdztyT7D+tz5tfCvH98PlXf6MjWudL8bbTAWU7OEpUVia2pcUlAOXkkOi0ANrx4Ieovmhw7G8/AC0Rn+g3hSf1A45RsODVFq9BezunWbjcNicwV2++/CFpE5fuXT6pRgrBfXgI3P4BVRMxaG4CXLl6uUPrg/8oYoz/uJtxzEwv767YeNICi9RfXjIg0hQLoZfIAZCxYIVZw9A91ZIG8+IP276gG1kHfyMfs2W95dK6Uy/fdF6G3p3PLFUtjSK6dZwQeO4IzltrxujQ26kgMFDYMmD7lDDI3JzLqXy969MpMd60iamXDpgxQ3okZpa7sd5TgtEH+aA8ia58bhSOwE= user@main.com`
+
+	httpmock.RegisterResponder("GET", "https://github.com/exampleUser.keys",
+		httpmock.NewStringResponder(200, payload))
+
+	// initial config without ssh_keys entry defined
+	conf, err := Load(filepath.Join("..", "testdata", "config.yaml"))
+	require.NoError(t, err)
+	assert.NotNil(t, conf)
+
+	// we expect the github keys to be added to the ssh keys defined in the config
+	configKeys := conf.CloudProviderConfig.ProviderSettings["ssh_keys"].([]string)
+	expected := append(configKeys, []string{"ae:dc:ab:c1:b1:b0:21:2b:8a:06:77:ae:9c:9b:4b:22", "a6:1e:2b:44:9c:84:e2:1c:84:9c:6d:2d:ed:72:ad:16"}...)
+
+	conf, err = UpdateConfigWithGithubKeys(conf)
+	assert.NoError(t, err)
+	assert.Contains(t, conf.CloudProviderConfig.ProviderSettings["ssh_keys"], expected)
+}
 
 func TestFetchGitHubKeys(t *testing.T) {
 	// Activate httpmock
@@ -20,7 +74,7 @@ ssh-rsa BBBBA3Nza... user@main.com`
 	httpmock.RegisterResponder("GET", "https://github.com/exampleUser.keys",
 		httpmock.NewStringResponder(200, payload))
 
-	keys, err := FetchGitHubKeys("exampleUser")
+	keys, err := fetchGitHubKeys("exampleUser")
 	assert.NoError(t, err)
 	assert.Len(t, keys, 2)
 	assert.Contains(t, keys[0], "ssh-rsa AAAAB3Nza")
@@ -36,7 +90,7 @@ func TestFetchGitHubKeys_NotFound(t *testing.T) {
 	httpmock.RegisterResponder("GET", "https://github.com/exampleUser.keys",
 		httpmock.NewStringResponder(404, "Not Found"))
 
-	_, err := FetchGitHubKeys("exampleUser")
+	_, err := fetchGitHubKeys("exampleUser")
 	assert.Error(t, err)
 }
 
@@ -49,21 +103,21 @@ func TestFetchGitHubKeys_Error(t *testing.T) {
 	httpmock.RegisterResponder("GET", "https://github.com/exampleUser.keys",
 		httpmock.NewErrorResponder(errors.New("internal server error")))
 
-	_, err := FetchGitHubKeys("exampleUser")
+	_, err := fetchGitHubKeys("exampleUser")
 	assert.Error(t, err)
 }
 
 func TestGenerateFingerprint(t *testing.T) {
 	key := "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCas+tCHuRci1xIHwkBFvrq/dDm0l3PSBiD+Pm7SOqq23Qg+ZUANcVNSgot0W/NmEHy/9rA78Ps+jHwrwPrliw6TNLYC82LueJHPo1dGioprmKVKn9efYVuFDUubRPr+/CAZXARUOSqMon7xiaxAy51qhIpWrLxcs2HP/G3IW8kVxuwdztyT7D+tz5tfCvH98PlXf6MjWudL8bbTAWU7OEpUVia2pcUlAOXkkOi0ANrx4Ieovmhw7G8/AC0Rn+g3hSf1A45RsODVFq9BezunWbjcNicwV2++/CFpE5fuXT6pRgrBfXgI3P4BVRMxaG4CXLl6uUPrg/8oYoz/uJtxzEwv767YeNICi9RfXjIg0hQLoZfIAZCxYIVZw9A91ZIG8+IP276gG1kHfyMfs2W95dK6Uy/fdF6G3p3PLFUtjSK6dZwQeO4IzltrxujQ26kgMFDYMmD7lDDI3JzLqXy969MpMd60iamXDpgxQ3okZpa7sd5TgtEH+aA8ia58bhSOwE="
 	expectedFingerprint := "a6:1e:2b:44:9c:84:e2:1c:84:9c:6d:2d:ed:72:ad:16"
-	fingerprint, err := GenerateFingerprint(key)
+	fingerprint, err := generateFingerprint(key)
 	assert.NoError(t, err)
 	assert.Equal(t, expectedFingerprint, fingerprint)
 
 	// check ed keys
 	key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH0p7vluS7ldSFDBYx9ZXVQcsJdWIoSTVvqhcakKDQ34"
 	expectedFingerprint = "ae:dc:ab:c1:b1:b0:21:2b:8a:06:77:ae:9c:9b:4b:22"
-	fingerprint, err = GenerateFingerprint(key)
+	fingerprint, err = generateFingerprint(key)
 	assert.NoError(t, err)
 	assert.Equal(t, expectedFingerprint, fingerprint)
 }
