68803f5680a70519d33eb6b0f9ac79e1673b91c4
Pere Villega <pvillega@users.noreply.github.com>
2024-02-14 18:18:20 +0100

    Pv/terraform s3 (#15)
    * Read config for terraform storage
    
    Signed-off-by: Pere Villega <pvillega@aracon.com>
    
    * Allow terraform to store state in an external s3 compatible store
    
    Tested with R2
    
    Signed-off-by: Pere Villega <pvillega@aracon.com>
    
    * Remove duplicate step
    
    Signed-off-by: Pere Villega <pvillega@aracon.com>
    
    * Replace values for TF config by env variables, if defined, so that we don't have to store in Github secrets for the S3 storage
    
    Signed-off-by: Pere Villega <pvillega@aracon.com>
    
    ---------
    
    Signed-off-by: Pere Villega <pvillega@aracon.com>

--- pkg/conf/config.go
+++ pkg/conf/config.go
@@ -13,10 +13,16 @@ type Config struct {
 	DC                  string                 `yaml:"dc_name"`
 	BaseDir             string                 `yaml:"base_dir"`
 	OrgName             string                 `yaml:"org_name"`
+	TfState             TerraformState         `yaml:"terraform_state"`
 	Providers           map[string]interface{} `yaml:"providers"`
 	CloudProviderConfig CloudProvider          `yaml:"cloud_provider_config"`
 	ServerGroups        map[string]ServerGroup `yaml:"server_groups"`
 	Services            map[string]interface{} `yaml:"services"`
+}
+
+type TerraformState struct {
+	Backend string            `yaml:"backend"`
+	Config  map[string]string `yaml:"config"`
 }
 
 type ServerGroup struct {
@@ -77,10 +83,25 @@ func Load(file string) (*Config, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	// replace any env vars in the Tf State config with their values
+	config.TfState = updateTerraformStateVarsConfig(config.TfState)
+
 	return &config, nil
 }
 
 func LoadTFExecVars() *tfexec.VarOption {
 	token := os.Getenv("HETZNER_TOKEN")
 	return tfexec.Var(fmt.Sprintf("hcloud_token=%s", token))
+}
+
+func updateTerraformStateVarsConfig(tfState TerraformState) TerraformState {
+	for key, value := range tfState.Config {
+		envValue, exists := os.LookupEnv(value)
+		// If the environment variable exists and is not an empty string, replace the value for that key in the map
+		if exists && envValue != "" {
+			tfState.Config[key] = envValue
+		}
+	}
+	return tfState
 }
