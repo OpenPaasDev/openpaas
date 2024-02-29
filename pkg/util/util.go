821f7070964c7014ff779e84d044da3cdf6635f9
Pere Villega <pvillega@aracon.com>
2024-02-29 08:54:09 +0100

    Fix #19 add command to install dependencies for OpenPaaS
    Signed-off-by: Pere Villega <pvillega@aracon.com>

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -1,11 +1,17 @@
 package util
 
 import (
+	"archive/tar"
+	"compress/gzip"
 	"context"
 	"encoding/json"
+	"fmt"
 	"io"
 	"math/rand"
 	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"time"
 )
 
@@ -56,4 +62,125 @@ func GetPublicIP(ctx context.Context) (string, error) {
 	}
 	// fmt.Print(ip.Query)
 	return ip.Query, nil
+}
+
+func RunCmd(cmd string, args ...string) error {
+	c := exec.Command(cmd, args...)
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stderr
+	err := c.Run()
+	if err != nil {
+		fmt.Println("Error running:", cmd, args, err)
+		return err
+	}
+	return nil
+}
+
+func IsBrewInstalled() bool {
+	err := RunCmd("brew", "--version")
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func IsPipInstalled() bool {
+	err := RunCmd("pip", "--version")
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func IsAnsibleInstalled() bool {
+	err := RunCmd("ansible", "--version")
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func IsHCloudInstalled() bool {
+	err := RunCmd("hcloud", "version")
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func ExtractTarGz(tempPath string, destPath string) error {
+	// Limit the size of reader output to 100 Mb to prevent decompression bombs
+	limitForReaders := int64(100 * 1024 * 1024)
+	file, err := os.Open(filepath.Clean(tempPath))
+	if err != nil {
+		fmt.Println("Error opening downloaded file:", err)
+		return err
+	}
+	defer file.Close() //nolint
+
+	limiter := io.LimitReader(file, limitForReaders)
+	uncompressedStream, err := gzip.NewReader(limiter)
+	if err != nil {
+		return err
+	}
+
+	tarLimiter := io.LimitReader(uncompressedStream, limitForReaders)
+	tarReader := tar.NewReader(tarLimiter)
+	for {
+		header, err := tarReader.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		// Construct the full path and create the file
+		path := filepath.Join(destPath, filepath.Clean(header.Name))
+		switch header.Typeflag {
+		case tar.TypeDir:
+			if err := os.MkdirAll(path, 0750); err != nil {
+				return err
+			}
+		case tar.TypeReg:
+			outFile, err := os.Create(filepath.Clean(path))
+			if err != nil {
+				return err
+			}
+			tarReaderLimiter := io.LimitReader(tarReader, limitForReaders)
+			if _, err := io.Copy(outFile, tarReaderLimiter); err != nil {
+				outFile.Close() // nolint
+				return err
+			}
+			outFile.Close() // nolint
+		}
+	}
+	return nil
+}
+
+func DownloadFile(ctx context.Context, url, outputPath string) error {
+	// Get the data
+	request, err := http.NewRequestWithContext(ctx, "GET", url, nil) // #nosec G107 called internally with known urls
+	if err != nil {
+		return err
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(request)
+	if err != nil {
+		fmt.Printf("Error sending request: %v", err)
+		return err
+	}
+	defer resp.Body.Close() // nolint
+
+	// Create the file
+	out, err := os.Create(filepath.Clean(outputPath))
+	if err != nil {
+		return err
+	}
+	defer out.Close() // nolint
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	return err
 }
--- pkg/util/util_test.go
+++ pkg/util/util_test.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"context"
+	"os"
 	"strings"
 	"testing"
 
@@ -27,4 +28,34 @@ func TestGetPublicIP(t *testing.T) {
 	ip, err := GetPublicIP(context.Background())
 	require.NoError(t, err)
 	assert.Equal(t, 3, strings.Count(ip, "."))
+}
+
+func Test_RunCmd(t *testing.T) {
+	err := RunCmd("ls", "-l")
+	require.NoError(t, err)
+}
+
+func Test_ExtractTarGZ(t *testing.T) {
+	tempPath := "../testdata/testfile.tar.gz"
+	destPath := "/tmp"
+	err := ExtractTarGz(tempPath, destPath)
+	require.NoError(t, err)
+	content, err := os.ReadFile(destPath + "/test.txt")
+	require.NoError(t, err)
+	text := string(content)
+	assert.Equal(t, "test", text)
+}
+
+func Test_DownloadFile(t *testing.T) {
+	ctx := context.Background()
+	url := "https://httpbin.org/robots.txt"
+	destPath := "/tmp/test-download.txt"
+	expectedContent := "User-agent: *\nDisallow: /deny\n"
+
+	err := DownloadFile(ctx, url, destPath)
+	require.NoError(t, err)
+	content, err := os.ReadFile(destPath)
+	require.NoError(t, err)
+	text := string(content)
+	assert.Equal(t, expectedContent, text)
 }
